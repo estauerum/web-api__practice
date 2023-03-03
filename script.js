f5eb97e055829a39ab7e4e18ca460d95a67be52f
Auurora <iodko_dasha@bk.ru>
2023-03-03 14:34:29 +0300

    using async+await

--- script.js
+++ script.js
@@ -8,17 +8,31 @@ const delay = function (ms) {
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
-function fetchTodods() {
-    console.log('fetch todo started')
-    return delay(2000)
-    .then(() => { fetch(url)
-    })
-    .then(response => response.json())
+// function fetchTodods() {
+//     console.log('fetch todo started')
+//     return delay(2000)
+//     .then(() => { fetch(url)
+//     })
+//     .then(response => response.json())
+// }
+
+
+// fetchTodods()
+//     .then(data => {
+//      console.log(data)
+// })
+//     .catch(e => { console.log(e)})
+
+async function fetchAcyncTodos () {
+    console.log('fetch todo started...')
+    try {
+        await delay(2000)
+        const response = await fetch(url)
+        const data = await response.json()
+         console.log(data)
+    } catch(e) {
+        console.log(e)
+    }
 }
 
-
-fetchTodods()
-    .then(data => {
-     console.log(data)
-})
-    .catch(e => { console.log(e)})
+fetchAcyncTodos()
